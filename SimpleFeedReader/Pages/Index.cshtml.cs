598380d6de16e5b0294254b1d0528d303c7ec9d1
Dennis <dennis.rathinam@yahoo.com>
2019-01-17 11:02:17 -0800

    Upgraded to V6

--- SimpleFeedReader/Pages/Index.cshtml.cs
+++ SimpleFeedReader/Pages/Index.cshtml.cs
@@ -34,7 +34,7 @@ namespace SimpleFeedReader.Pages
                 }
                 catch (UriFormatException)
                 {
-                    ErrorText = "There was a problem parsing the URL.";
+                    ErrorText = "There was a problem parsing the URL or the URL doesn't exist.";
                     return;
                 }
                 catch (WebException ex) when (ex.Status == WebExceptionStatus.NameResolutionFailure)
